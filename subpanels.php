d85cff0c7015cef630aa29030ff0b69cd7eb3ccb
Jairo Watts <jwatts.pajaro@gmail.com>
2025-03-20 19:33:09 -0500

    Add files via upload

---
+++ subpanels.php
@@ -0,0 +1,9 @@
+<?php
+
+$layout_defs['Contacts']['subpanel_setup']['gmail_sync'] = array(
+    'order' => 100,
+    'module' => 'GmailSync',
+    'subpanel_name' => 'default',
+    'title_key' => 'LBL_GMAIL_SYNC',
+    'get_subpanel_data' => 'gmail_sync',
+);
